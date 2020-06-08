334a8c482cf7940610ec8eda025dd8b1654dc3c9
Christian Flach <cmfcmf.flach@gmail.com>
2020-06-08 22:12:43 +0200

    Whitelist files in package.json

---
+++ cypress/integration/create-gif.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+it("create README gif", () => {
+  cy.viewport(1000, 600);
+  cy.clearViewport();
+
+  cy.visit("https://cmfcmf.github.io/OpenWeatherMap-PHP-API");
+  cy.wait(3000);
+  cy.get("#search_input_react")
+    .focus()
+    .wait(500)
+    .type("php{enter}", { delay: 750 });
+  cy.wait(1000);
+  cy.get("#search_input_react")
+    .focus()
+    .wait(500)
+    .type("api{enter}", { delay: 750 });
+  cy.wait(3000);
+});
