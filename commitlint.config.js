36b0c570577f1af22122b46a8b2e733177a1f1f7
Christian Flach <cmfcmf.flach@gmail.com>
2023-02-25 17:37:01 +0100

    chore: Add commit message linter

---
+++ commitlint.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "subject-case": [2, "always", "sentence-case"],
+  },
+  helpUrl:
+    "https://github.com/cmfcmf/docusaurus-search-local/blob/main/CONTRIBUTING.md#commit-message-guidelines",
+};
