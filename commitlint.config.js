18fdf5212957799580d4b2a135a868905eb6efb1
Christian Flach <cmfcmf.flach@gmail.com>
2025-10-12 10:07:46 +0200

    chore: Do not commitlint dependabot commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,6 +3,7 @@ module.exports = {
   rules: {
     "subject-case": [2, "always", "sentence-case"],
   },
+  ignores: [(commit) => commit.includes("Signed-off-by: dependabot[bot]")],
   helpUrl:
     "https://github.com/cmfcmf/docusaurus-search-local/blob/main/CONTRIBUTING.md#commit-message-guidelines",
 };
